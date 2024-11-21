562a893b50ffb1c36620e1000861cc2d3b323e5d
Ashlyn Knox <ashlyn@lilyx.io>
2024-11-21 10:42:11 -0700

    serve static files

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use("/assets", express.static(path.join(__dirname, "/assets")));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on ${port}`);
+  console.log(`Server listening on http://localhost:${port}`);
 });
