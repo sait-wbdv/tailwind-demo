f90b3c3a92b19c4bd22333dbd2ef92db69d1a03c
Ashlyn Knox <ashlyn@lilyx.io>
2024-11-21 10:54:29 -0700

    error handling

--- index.js
+++ index.js
@@ -9,12 +9,25 @@ app.use("/assets", express.static(path.join(__dirname, "/assets")));
 const options = {
   root: path.join(__dirname, "public"),
 };
-app.get("/", (req, res) => {
-  res.sendFile("index.html", options);
+app.get("/", (req, res, next) => {
+  try {
+    if (!res.ok) {
+      throw new Error("Something went wrong");
+    }
+    res.sendFile("index.html", options);
+  } catch (err) {
+    next(err);
+  }
 });
+
 app.get("/about", (req, res) => {
   res.sendFile("about.html", options);
 });
+
+app.use((req, res, next) => {
+  res.status(404).send("Page Not Found");
+});
+
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
 });
