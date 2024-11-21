beaaab268313ff438d767ed17c28e89fe5208302
Ashlyn Knox <ashlyn@lilyx.io>
2024-11-21 10:11:21 -0700

    served index.html file at root

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 "use strict";
-
+const path = require("node:path");
 const express = require("express");
 
 const app = express();
+const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello there!");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(5500, () => {
-  console.log("Server listening on 5500");
+app.listen(3000, () => {
+  console.log(`Server listening on ${3000}`);
 });
