e4de2ad3f9b9c3ae2d1c968de3318b9517237630
Ashlyn Knox <ashlyn@lilyx.io>
2024-11-21 10:48:43 -0700

    serve about page as well

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const app = express();
 const port = 3000;
 
 app.use("/assets", express.static(path.join(__dirname, "/assets")));
-
+const options = {
+  root: path.join(__dirname, "public"),
+};
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile("index.html", options);
 });
-
+app.get("/about", (req, res) => {
+  res.sendFile("about.html", options);
+});
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
 });
